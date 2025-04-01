2ab1f07ac665937d637f79a241a52f604976d67e
Gabriel Cosato <gabrielcosato@gmail.com>
2025-03-31 22:26:49 -0300

    findById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,10 +1,15 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
+
+import java.io.FileNotFoundException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class DaoFactory {
 
-    public static SellerDao createSellerDao(){
-        return new SellerDaoJDBC();
+    public static SellerDao createSellerDao() throws SQLException, FileNotFoundException {
+        return new SellerDaoJDBC(DB.getConnection());
     }
 }
