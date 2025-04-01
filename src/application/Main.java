effe595d9bdcb4e2d5628b91273b82bbaf3fcc8e
Gabriel Cosato <gabrielcosato@gmail.com>
2025-03-31 21:52:18 -0300

    Seller entity class

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,11 +4,13 @@ import db.DB;
 import db.DBIntegrityException;
 import db.DbException;
 import model.entities.Department;
+import model.entities.Seller;
 
 import java.io.FileNotFoundException;
 import java.sql.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -16,6 +18,8 @@ public class Main {
     public static void main(String[] args) throws SQLException, FileNotFoundException {
 
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
+
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+        System.out.println(seller);
     }
 }
