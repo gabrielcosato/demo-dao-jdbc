9a91b155944aeeb8a3c455cb6831165878952aa1
Gabriel Cosato <gabrielcosato@gmail.com>
2025-03-31 23:28:44 -0300

    insert implementation

--- src/application/Main.java
+++ src/application/Main.java
@@ -43,5 +43,12 @@ public class Main {
             System.out.println(obj1);
         }
 
+        System.out.println("\n=== TEST 4: seller insert =====");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, dep);
+
+        sellerDao.insert(newSeller);
+
+        System.out.println("Inserted! New id = " + newSeller.getId());
+
     }
 }
